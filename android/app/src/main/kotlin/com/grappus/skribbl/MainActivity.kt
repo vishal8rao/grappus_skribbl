b241e13ff7561b4d5df83c6ac6237cba65266ccb
Noor Bhatia <bhatia.noor01@gmail.com>
2023-08-08 15:08:11 +0530

    setup repo

---
+++ android/app/src/main/kotlin/com/grappus/skribbl/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.grappus.skribbl.grappus_skribbl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:grappus_skribbl/app/app.dart';
+import 'package:grappus_skribbl/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
