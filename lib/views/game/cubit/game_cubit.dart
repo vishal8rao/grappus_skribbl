8cf15b5f539168784b614b0721ffb20c742229e2
mox <moksh@grappus.com>
2023-08-23 09:38:43 +0530

    code format and added room model

--- lib/views/game/cubit/game_cubit.dart
+++ lib/views/game/cubit/game_cubit.dart
@@ -3,8 +3,10 @@ import 'dart:async';
 import 'package:api/session/bloc/session_bloc.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:game_repository/game_repository.dart';
 import 'package:player_repository/models/drawing_points.dart';
+import 'package:player_repository/models/room.dart';
 
 part 'game_state.dart';
 
@@ -14,22 +16,35 @@ class GameCubit extends Cubit<GameState> {
         super(const GameState());
 
   final GameRepository _gameRepository;
-  StreamSubscription<SessionState>? _sessionStateSub;
+  StreamSubscription<Room>? _roomStreamSubscription;
 
   Future<void> connect() async {
-    _sessionStateSub = _gameRepository.session.listen((sessionState) {
-      emit(state.copyWith(sessionState: sessionState));
-    });
+    debugPrint('Connect');
+    try {
+      _roomStreamSubscription = _gameRepository.session.listen((room) {
+        emit(state.copyWith(room: room));
+      });
+    } catch (e) {
+      print('Session state subscription error: $e');
+    }
   }
 
   Future<void> addPoints(DrawingPointsWrapper points) async {
-    _gameRepository.sendPoints(points);
+    try {
+      _gameRepository.sendPoints(points);
+    } catch (e) {
+      print('Sending points error from GameCubit: $e');
+    }
   }
 
   @override
   Future<void> close() async {
-    await _sessionStateSub?.cancel();
-    _gameRepository.close();
+    await _roomStreamSubscription?.cancel();
+    try {
+      _gameRepository.close();
+    } catch (e) {
+      print('Closing connection error from GameCubit: $e');
+    }
     return super.close();
   }
 }
